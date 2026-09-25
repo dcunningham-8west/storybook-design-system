54d396b72d861c9615897bde1afeaa74ca59f155
Diarmuid Cunningham <dcunningham@8west.ie>
2026-09-25 15:32:11 +0100

    CardButton added as story 6

--- src/components/CTAButton/CTAButton.stories.tsx
+++ src/components/CTAButton/CTAButton.stories.tsx
@@ -13,10 +13,50 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Primary: Story = {
   args: {
     label: 'Learn More',
     size: 'small',
     isSecondary: false,
+  },
+};
+
+export const PrimarytMedium: Story = {
+  args: {
+    label: 'Learn More',
+    size: 'medium',
+    isSecondary: false,
+  },
+};
+
+export const PrimaryLarge: Story = {
+  args: {
+    label: 'Learn More',
+    size: 'large',
+    isSecondary: false,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Learn More',
+    size: 'small',
+    isSecondary: true,
+  },
+};
+
+export const SecondaryMedium: Story = {
+  args: {
+    label: 'Learn More',
+    size: 'medium',
+    isSecondary: true,
+  },
+};
+
+export const SecondaryLarge: Story = {
+  args: {
+    label: 'Learn More',
+    size: 'large',
+    isSecondary: true,
   },
 };
